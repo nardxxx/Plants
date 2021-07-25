6986d6e672182a6dda76af3cc81e84aa7a58b72b
nardxxx <76057779+nardxxx@users.noreply.github.com>
2021-07-25 10:32:25 +0300

    Add files via upload

---
+++ PrismaticPlants/js/animate-css.js
@@ -0,0 +1,31 @@
+//Animate CSS + WayPoints javaScript Plugin
+//Example: $(".element").animated("zoomInUp", "zoomOutDown");
+(function ($) {
+	$.fn.animated = function (inEffect, outEffect) {
+		var jQueryObject = $(this);
+		inEffect = 'animate__' + inEffect;
+		outEffect = 'animate__' + outEffect;
+		jQueryObject.css("opacity", "0").addClass("animate__animated");
+
+		jQueryObject.waypoint(function (dir) {
+			if (dir === "down") {
+				jQueryObject.removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+			} else {
+				jQueryObject.removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+			};
+		}, {
+			offset: "80%"
+		});
+
+		jQueryObject.waypoint(function (dir) {
+			if (dir === "down") {
+				jQueryObject.removeClass(inEffect).addClass(outEffect).css("opacity", "1");
+			} else {
+				jQueryObject.removeClass(outEffect).addClass(inEffect).css("opacity", "1");
+			};
+		}, {
+			offset: -$(window).height()
+		});
+	};
+})(jQuery);
+
